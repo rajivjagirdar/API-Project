5e64285318fcb1dee97a79cdc87bdf02edab3169
Kirby <legendofkirby85@gmail.com>
2018-10-10 18:31:53 -0700

    omdb display done

--- assets/javascript/omdb.js
+++ assets/javascript/omdb.js
@@ -1,68 +1,70 @@
 $( document ).ready(function() {
-    // This function handles events where a movie button is clicked
-    $("#add-movie").on("click", function(event) {
-        event.preventDefault();
 
+    // Create function that will render information into HTML Display
+    function displayMovieInfo (movie) { 
 
-            // This line grabs the input from the textbox
-            var movie = $("#movie-input").val().trim();
-            var queryURL = "http://www.omdbapi.com/?t=" + movie + "&apikey=45885583&limit=5";
+        // Querying the oMDB api for the selected movie
+        var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=45885583&limit=5";
 
-            // Creating an AJAX call for the specific movie button being clicked
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(response) {
-             console.log(response)
+        // Creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+        console.log(response)
 
-        // Movies will get dumped into display section in HTML (figure out)
-        // Create function that will re-render information into HTML Display
-        function displayMovieInfo { 
+            // Storing the Title data
+            var title = response.Title;
+            // Creating an element to have the rating displayed
+            var pTitle = $("<p>").text("Title: " + title);
+ 
+
+            // Storing the rating data
+            var rating = response.Rated;
+            // Creating an element to have the rating displayed
+            var pRating = $("<p>").text("Rating: " + rating);
+ 
+    
+            // Storing the release year
+            var released = response.Released;
+            // Creating an element to hold the release year
+            var pReleased = $("<p>").text("Released: " + released);
+
+            // Storing the plot
+            var plot = response.Plot;
+            // Creating an element to hold the plot
+            var pPlot = $("<p>").text("Plot: " + plot);
+
+            // Storing the cast
+            var cast = response.Actors
+            // Creating an element to hold the cast
+            var pCast = $("<p>").text("Cast: " + cast);
+
+            // Storing the poster
+            var poster = response.Poster
+            // Creating an element to hold the cast
+            var pPoster = $("<img>").attr("src", poster);
+
 
             // Deleting the movie omdb display prior to adding new movie
-            // (this is necessary otherwise we will have repeat buttons)
-            $(".omdb").empty();
+            $("#omdb").empty();
+            //Displaying all the response variables
+            $("#omdb").append(pTitle, pRating, pReleased, pCast, pPlot, pPoster)
 
+            });
+        };    
+                    
+        // This function handles events where a movie button is clicked
+        $("#add-movie").on("click", function(event) {
+        event.preventDefault();
 
-                // Storing the rating data
-                var rating = response.Rated;
-  
-                // Creating an element to have the rating displayed
-                var pOne = $("<p>").text("Rating: " + rating);
-  
-                // Displaying the rating
-                $(".omdb").append(pOne);
-  
-                // Storing the release year
-                var released = response.Released;
-  
-                // Creating an element to hold the release year
-                var pTwo = $("<p>").text("Released: " + released);
-  
-                // Displaying the release year
-                $(".omdb").append(pTwo);
-  
-                // Storing the plot
-                var plot = response.Plot;
-  
-                // Creating an element to hold the plot
-                var pThree = $("<p>").text("Plot: " + plot);
-  
-                // Appending the plot
-                $(".omdb").append(pThree);
-
-                // Storing the cast
-                var cast = response.Actors
-
-                // Creating an element to hold the cast
-                var pFour = $("<p>").text("Cast: " + cast);
-
-                // Displaying the cast 
-                $(".omdb").append(pFour);
-            };
+        // This line grabs the input from the textbox
+        var movie = $("#movie-input").val().trim();
+    
+    //Running the display movie function
+    displayMovieInfo(movie);
         });
-    });
         
 });
 
-displayMovieInfo();
+
