6fa9c8a57483f0fe0e977c4d72c0293f987733af
Freddy Fazbear <talaifreddie@gmail.com>
2018-10-13 10:42:04 -0700

    apitestnyt

---
+++ NYTgroupProject/assets/javascript/NYT2.js
@@ -0,0 +1,68 @@
+$( document ).ready(function() {
+
+    // Create function that will render information into HTML Display
+    function displayMovieInfo (movie) { 
+
+        // Querying the NYT api for the selected movie
+        var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=45885583&limit=5";
+
+        // Creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+        console.log(response)
+
+            // Storing the Title data
+            var title = response.Title;
+            // Creating an element to have the rating displayed
+            var pTitle = $("<p>").text("Title: " + title);
+ 
+
+            // Storing the rating data
+            var rating = response.Rated;
+            // Creating an element to have the rating displayed
+            var pRating = $("<p>").text("Rating: " + rating);
+ 
+    
+            // Storing the release year
+            var released = response.Released;
+            // Creating an element to hold the release year
+            var pReleased = $("<p>").text("Released: " + released);
+
+            // Storing the plot
+            var plot = response.Plot;
+            // Creating an element to hold the plot
+            var pPlot = $("<p>").text("Plot: " + plot);
+
+            // Storing the cast
+            var cast = response.Actors
+            // Creating an element to hold the cast
+            var pCast = $("<p>").text("Cast: " + cast);
+
+            // Storing the poster
+            var poster = response.Poster
+            // Creating an element to hold the cast
+            var pPoster = $("<img>").attr("src", poster);
+
+
+            // Deleting the movie omdb display prior to adding new movie
+            $("#omdb").empty();
+            //Displaying all the response variables
+            $("#omdb").append(pTitle, pRating, pReleased, pCast, pPlot, pPoster)
+
+            });
+        };    
+                    
+        // This function handles events where a movie button is clicked
+        $("#add-movie").on("click", function(event) {
+        event.preventDefault();
+
+        // This line grabs the input from the textbox
+        var movie = $("#movie-input").val().trim();
+    
+    //Running the display movie function
+    displayMovieInfo(movie);
+        });
+        
+});
---
+++ NYTgroupProject/assets/javascript/NYT3.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+    // Create function that will render information into HTML Display
+    function displayMovieInfo (movie) { 
+
+        // Querying the oMDB api for the selected movie
+        var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+        url += '?' + $.param({
+          'api-key': "b2f7ed234f2647ea823d27075613c4f6",
+          "query" : movie
+          
+        });
+        
+        
+        $.ajax({
+          url: url,
+          method: 'GET',
+        }).done(function(result) {
+
+          var title = result.results[0].headline;
+          var pTitle = $("<p>").text("Title: " + title);
+          console.log(title);
+          
+          var author = result.results[0].byline;
+          var pAuthor = $("<p>").text("Author: " + author);
+          console.log(author);
+          
+          var snipit = result.results[0].summary_short;
+          var pSnipit = $("<p>").text("Snip It: " + snipit);
+          console.log(snipit);
+
+          var link = result.results[0].link.url;
+          var pLink = $("<p>").text("Link: " + link);
+          console.log(link);
+
+          // // Deleting the movie NYTReview display prior to adding new review
+          $("#nyTimes").empty();
+          // //Displaying all the response variables
+          $("#nyTimes").append(pTitle, pAuthor, pSnipit, pLink)
+          
+        }).fail(function(err) {
+          throw err;
+        });
+      };
+                    
+        // This function handles events where a movie button is clicked
+        $("#add-movie").on("click", function(event) {
+        event.preventDefault();
+
+        // This line grabs the input from the textbox
+        var movie = $("#movie-input").val().trim();
+    
+    //Running the display movie function
+    displayMovieInfo(movie);
+        });
+        
+});
